071388b702be55e707526ecf5151645baa4f1906
kian1991 <kianluetke@gmail.com>
2024-06-04 12:40:14 +0200

    feature(middleware): added rate limits

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { rateLimit } from 'express-rate-limit'
 import {
   productRouter,
   warehouseRouter,
@@ -14,8 +15,18 @@ import 'dotenv/config'
 
 const app = express()
 
+// Middlewares
 app.use(cors({ origin: true }))
 app.use(express.json())
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 // Routes
 app.use('/warehouses', warehouseRouter)
